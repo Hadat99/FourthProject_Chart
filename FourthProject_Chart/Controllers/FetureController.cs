6c4f4d7eed3daf231807b20f714167bf092867e8
Dat <dathm@rikkeisoft.com>
2022-05-19 11:24:52 +0700

    add new feature file

---
+++ FourthProject_Chart/Controllers/FetureController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FourthProject_Chart.Controllers
+{
+    public class FetureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
