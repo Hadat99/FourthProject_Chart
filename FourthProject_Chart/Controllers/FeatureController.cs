ea86611166dccdcda9cba9e7ead80feb226365a5
Dat <dathm@rikkeisoft.com>
2022-05-19 11:30:15 +0700

    add FeatureController

---
+++ FourthProject_Chart/Controllers/FeatureController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FourthProject_Chart.Controllers
+{
+    public class FeatureController : Controller
+    {
+        public IActionResult Feature()
+        {
+            int a = 1 + 2;
+            return View();
+        }
+    }
+}
